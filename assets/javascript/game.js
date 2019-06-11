5e90592fdc7f9d2e52a4ce3f524bb47a560d3668
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-06-11 00:44:16 -0700

    added javascript code to attempt to create a working intro screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,13 @@ const game = function(){
     let guessesLeft = 6;
 
     const startGame = function(){
-        const playBtn = document.querySelector(".intro button");
-        const introScreen = document.querySelector(".intro");
-        const game = document.querySelector(".game");
+        const playBtn = document.querySelector(".game button");
+        const introScreen = document.querySelector(".game");
+        const game = document.querySelector(".gametime");
 
         playBtn.addEventListener("click", function(){
-            introScreen.classList.add("fadeOut");
-            game.classList.add("fadeIn");
+            gameScreen.classList.add("fadeOut");
+            gametime.classList.add("fadeIn");
         });
     };
 
