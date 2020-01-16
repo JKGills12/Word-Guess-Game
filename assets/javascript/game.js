afd7c5a64ff9b6fe7d8b64ce69e9eb58cade4a58
D J <jkgills12@gmail.com>
2020-01-16 13:47:08 -0800

    updated heading title

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,19 @@
 window.onload = function () {
 	var letters = document.getElementById("letters").children
 	for (letter of letters) {
-    letter.addEventListener("click", function(e){ 
-    	e.target.disabled = 'true'
-    	play(this.innerText)
-    	
-    })
-  }
+		letter.addEventListener("click", function (e) {
+			e.target.disabled = 'true'
+			play(this.innerText)
+
+		})
+	}
 }
 
 //Array of words
 var words = ['SWIMSUIT', 'POPSICLE', 'BEACHBALL', 'SANDCASTLE', 'SUP', 'SANDALS', 'BIKING', 'SWIMMING', 'TANNING', 'SHORTS', 'VOLLEYBALL', 'RIBS', 'HAMBURGERS', 'SMORES', 'GOGGLES', 'SNORKEL', 'SUNGLASSES']
 
 //Array of categories
-var categories = ['Beachwear','Actvity','Food']
+var categories = ['Beachwear', 'Actvity', 'Food']
 
 //Get random word + category
 var random = Math.random()
@@ -25,19 +25,19 @@ var hint = categories[Math.floor(random * words.length)]
 var answerArray = []
 
 //Replace letter with blank at each index
-for (let i = 0; i < word.length; i++){
+for (let i = 0; i < word.length; i++) {
 	answerArray[i] = "_"
 }
 
 //Get elements by id
-function id(str){
+function id(str) {
 	return document.getElementById(str)
 }
 
 //Keys press
 function words() {
-    var words = document.getElementById("");
-  }
+	var words = document.getElementById("");
+}
 
 //Declare variables for elements
 var guessWord = id('guessWord')
@@ -57,16 +57,16 @@ var maxTries = 10
 //Show amount of tries 
 triesLeft.innerHTML = "<strong>Tries Left:</strong><br>" + maxTries
 
-function play(letter){
-	let found = false	
+function play(letter) {
+	let found = false
 	// The Game Loop
 	if (remainingLetters > 0) {
 		//Update inner HTML with answer
 		guessWord.innerTextHTML = answerArray
 		//Guess equals button letter
 		var guess = letter
-		for (let j = 0; j < word.length; j++){
-			if (word[j] === guess){
+		for (let j = 0; j < word.length; j++) {
+			if (word[j] === guess) {
 				answerArray[j] = guess
 				remainingLetters--
 				found = true
@@ -74,22 +74,22 @@ function play(letter){
 			// *WIN* Reload
 			if (remainingLetters === 0) {
 				winLose.innerHTML = "YOU GOT IT!"
-				setTimeout(function(){
+				setTimeout(function () {
 					window.location.reload()
 				}, 2000)
 			}
 		}
 	}
 	//Decrease number of max tries if chosen letter is incorrect
-	if (!found){
+	if (!found) {
 		maxTries--
 	}
 	// *LOSE* Reload
 	if (maxTries === 0) {
 		winLose.innerHTML = "SORRY, YOU LOST :( BETTER LUCK NEXT TIME"
-		setTimeout(function(){
-					window.location.reload()
-				}, 2000)
+		setTimeout(function () {
+			window.location.reload()
+		}, 2000)
 	}
 
 	triesLeft.innerHTML = "<strong>Tries Left:</strong><br>" + maxTries
